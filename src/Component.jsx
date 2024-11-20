031d75e4dbfd2b360699c64c371ec6273f8a29c3
Yenny <yennycarolinadurango@gmail.com>
2024-11-19 20:52:31 -0500

    terminado

--- src/Component.jsx
+++ src/Component.jsx
@@ -3,16 +3,21 @@ import React, { useState } from "react"
 export default function Component() {
 
     const [text, setText] = useState()
-    
+    const [update, setUpdate] = useState()
+
     const textOnChange = (event) => {
         setText(event.target.value)
     }
+    const buttonOnClick = () => {
+      setUpdate(text)
+    }
+
     return (
         <div>
             <input type="text" value={text} onChange={textOnChange} />
-            <button>Actualizar</button>
+            <button onClick={buttonOnClick}>Actualizar</button>
             <p>Texto input: {text}</p>
-            <p>Texto actualizado</p>
+            <p>Texto actualizado: {update}</p>
         </div>
     )
 }
